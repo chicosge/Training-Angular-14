ad31c73f026a2ed9659a56a8001cfef83603db65
chicosge <124004964+chicosge@users.noreply.github.com>
2023-02-07 21:01:37 -0600

    [06-02-2023 task 1] examples of arrow function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,21 @@ export class AppComponent {
 
   }
 
+  //Arrow functions
+
+//Se utilizan para minimizar el codigo a utilizar. en este tipo de funciones se puede omitir ciertas cosas para eficientar el codigo, por ejemplo:
+
+//const getName = (name: string) => { return "hi" + name;}
+
+//Tambien podemos omitir las llaves y la palabra return si solo es una unica ejecucion. 
+//const getName = (name: string) => "hi" + name;
+
+//Cuando no tenemos parametros de entrada
+//const getName = () => "hello";
+
+//Tambien podemos omitir parentesis del parametro de entrada
+//const mult = num => num *2;
+
   private mult(num) {
     return num;
   }
